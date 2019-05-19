ae44b88fb7f95a9d96ce5ccc2544697cc2932b09
Auchaphon Phuengsema <auchaphonphuengsema@Auchaphons-MacBook-Pro.local>
2019-05-19 09:53:15 +0700

    first commit

---
+++ src/main/java/com/auchaphon/example/springapi/services/MemberService.java
@@ -0,0 +1,44 @@
+package com.auchaphon.example.springapi.services;
+import com.auchaphon.example.springapi.models.Member;
+import com.auchaphon.example.springapi.repositories.MemberRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service("memberService")
+public class MemberService {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public Member findMemberByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public Member updateMember(Member member, Long id) {
+        Member updateMember = memberRepository.findById(id).orElse(null);
+        if (updateMember != null) {
+            updateMember.setFirstName(member.getFirstName());
+            updateMember.setLastName(member.getLastName());
+        }
+        final Member mymember = memberRepository.save(updateMember);
+        return mymember;
+    }
+
+    public Boolean deleteMember(Long id) {
+        Member delMember = memberRepository.findById(id).orElse(null);
+        if (delMember != null) {
+            memberRepository.delete(delMember);
+            return true;
+        }
+        return false;
+    }
+}
